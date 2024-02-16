dbaacd0b36fa4c1aadbc43a66af15721acc7ba2f
Daniil Kiselev <danqkiselev@yandex.ru>
2024-02-16 21:50:09 +0300

    Added input processing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
+import { PromptService } from './core/prompt/prompt.service';
+
 class App {
-    run() {
-        console.log('Done');
+    async run() {
+        const res = await (new PromptService()).input<number>('Число', 'number')
+        console.log(res)
     }
 }
-const app = new App()
-app.run()
+const app = new App();
+app.run();
